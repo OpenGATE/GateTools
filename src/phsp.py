bedd50fd5a3f369bf4ea759f568d4565146026ad
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2019-03-28 16:39:36 +0100

    add file size

--- src/phsp.py
+++ src/phsp.py
@@ -97,3 +97,15 @@ def load_npy(filename, nmax=-1):
     return data, x.dtype.names, n
 
 
+
+''' ---------------------------------------------------------------------------
+ https://stackoverflow.com/questions/14996453/python-libraries-to-calculate-human-readable-filesize-from-bytes
+'''
+def humansize(nbytes):
+    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s %s' % (f, suffixes[i])
