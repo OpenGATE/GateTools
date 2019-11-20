cc39f0eff7bdf50cab30059e48923a84ac6ac1cc
David Boersma <david.boersma@medaustron.at>
2019-11-20 12:20:21 +0100

    import logging configuration function

--- gatetools/__init__.py
+++ gatetools/__init__.py
@@ -9,3 +9,4 @@ from .roi_utils import *
 from .bounding_box import *
 from .image_crop import *
 from .pbs_plan_file import dicom_rt_pbs_plan_to_gate_conversion
+from .logging_conf import logging_conf
