4678be57df6e90cef931b4fd7b8e0da7850675d6
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2020-11-16 14:56:43 +0100

    In image stats, add histogram output

--- gatetools/image_statistics.py
+++ gatetools/image_statistics.py
@@ -15,7 +15,7 @@ import numpy as np
 import logging
 logger=logging.getLogger(__name__)
 
-def imageStatistics(input=None, mask=None, resample=False):
+def imageStatistics(input=None, mask=None, resample=False, histogramBins=1000):
 
     if input is None:
         logger.error("Set an input")
@@ -54,6 +54,7 @@ def imageStatistics(input=None, mask=None, resample=False):
     outputStats["mean"] = np.mean(outputStats["sum"]/outputStats["nbPixel"])
     outputStats["variance"] = np.var(inputArray)
     outputStats["sigma"] = np.sqrt(outputStats["variance"])
+    outputStats["hist"] = np.histogram(inputArray, histogramBins)
 
     return outputStats
     
@@ -81,7 +82,7 @@ class Test_Image_Statistics(LoggedTestCase):
     def test_image_statistics(self):
         logger.info('Test_Image_Statistics test_image_statistics')
         image = createImageExample()
-        outputStats = imageStatistics(input=image)
+        outputStats = imageStatistics(input=image, histogramBins=10)
         self.assertTrue(outputStats["nbPixel"] == 12420)
         self.assertTrue(outputStats["mean"] == -0.5)
         self.assertTrue(outputStats["median"] == -0.5)
@@ -90,6 +91,8 @@ class Test_Image_Statistics(LoggedTestCase):
         self.assertTrue(outputStats["maximum"] == 9)
         self.assertTrue(outputStats["variance"] == 33.25)
         self.assertTrue(outputStats["sigma"] == 5.766281297335398)
+        self.assertTrue(np.allclose(outputStats["hist"][0], [1242, 1242, 1242, 1242, 1242, 1242, 1242, 1242, 1242, 1242]))
+        self.assertTrue(np.allclose(outputStats["hist"][1], [-10. , -8.1, -6.2, -4.3, -2.4, -0.5, 1.4, 3.3, 5.2, 7.1, 9.]))
 
     def test_image_statistics_with_mask(self):
         logger.info('Test_Image_Statistics test_image_statistics_with_mask')
@@ -105,7 +108,7 @@ class Test_Image_Statistics(LoggedTestCase):
         mask = itk.image_from_array(mask)
         mask.SetOrigin([7, 3.4, -4.6])
         mask.SetSpacing([4, 2, 3.6])
-        outputStats = imageStatistics(input=image, mask=mask)
+        outputStats = imageStatistics(input=image, mask=mask, histogramBins=5)
         self.assertTrue(outputStats["nbPixel"] == 621)
         self.assertTrue(outputStats["mean"] == 1)
         self.assertTrue(outputStats["median"] == 1)
@@ -114,6 +117,8 @@ class Test_Image_Statistics(LoggedTestCase):
         self.assertTrue(outputStats["maximum"] == 1)
         self.assertTrue(outputStats["variance"] == 0)
         self.assertTrue(outputStats["sigma"] == 0)
+        self.assertTrue(np.allclose(outputStats["hist"][0], [0, 0, 621, 0, 0]))
+        self.assertTrue(np.allclose(outputStats["hist"][1], [0.5, 0.7, 0.9, 1.1, 1.3, 1.5]))
 
     def test_image_statistics_with_mask_and_resample(self):
         logger.info('Test_Image_Statistics test_image_statistics_with_mask')
@@ -129,7 +134,7 @@ class Test_Image_Statistics(LoggedTestCase):
         mask = itk.image_from_array(mask)
         mask.SetOrigin([3, 3.4, -4.6])
         mask.SetSpacing([4, 2, 3.6])
-        outputStats = imageStatistics(input=image, mask=mask, resample=True)
+        outputStats = imageStatistics(input=image, mask=mask, resample=True, histogramBins=7)
         self.assertTrue(outputStats["nbPixel"] == 594)
         self.assertTrue(outputStats["mean"] == 1)
         self.assertTrue(outputStats["median"] == 1)
@@ -138,3 +143,5 @@ class Test_Image_Statistics(LoggedTestCase):
         self.assertTrue(outputStats["maximum"] == 1)
         self.assertTrue(outputStats["variance"] == 0)
         self.assertTrue(outputStats["sigma"] == 0)
+        self.assertTrue(np.allclose(outputStats["hist"][0], [0, 0, 0, 594, 0, 0, 0]))
+        self.assertTrue(np.allclose(outputStats["hist"][1], [0.5, 0.64285714, 0.78571429, 0.92857143, 1.07142857, 1.21428571, 1.35714286, 1.5]))
