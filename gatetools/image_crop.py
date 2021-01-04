c46f638faa0bdaae1026bbbe7bbd6cf04c22195c
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2021-01-04 16:11:17 +0100

    correct bug

--- gatetools/image_crop.py
+++ gatetools/image_crop.py
@@ -11,19 +11,13 @@ This module provides a function to crop image
 
 """
 
-# -----------------------------------------------------------------------------
-#   Copyright (C): OpenGATE Collaboration
-#   This software is distributed under the terms
-#   of the GNU Lesser General  Public Licence (LGPL)
-#   See LICENSE.md for further details
-# -----------------------------------------------------------------------------
-
-
 import itk
 import gatetools as gt
 import numpy as np
 import logging
-logger=logging.getLogger(__name__)
+
+logger = logging.getLogger(__name__)
+
 
 def image_auto_crop(img, bg=0):
     """
@@ -54,52 +48,50 @@ def image_auto_crop(img, bg=0):
 
     # special case for negative value
     img_array = itk.array_view_from_image(img)
-    minv = np.min(img_array)
+    minv = int(np.min(img_array))
+    maxv = int(np.max(img_array))
     bg = int(bg)
-    if minv<0:
+    if minv < 0:
         img_array -= minv
         bg -= minv
     # force to integer
     bg = int(bg)
-    #print(itk.LabelImageToLabelMapFilter.GetTypes())
-    #print(itk.StatisticsLabelObject.GetTypes())
+    # print(itk.LabelImageToLabelMapFilter.GetTypes())
+    # print(itk.StatisticsLabelObject.GetTypes())
 
-    # Cast to an image type which is compatible (ushort)
+    # Force cast to an image type which is compatible (ushort)
     OutputPixelType = itk.ctype('unsigned short')
     OutputImageType = itk.Image[OutputPixelType, Dim]
-    caster = itk.CastImageFilter[ImageType, OutputImageType].New()
-    caster.SetInput(img)
-    caster.Update()
-    img = caster.GetOutput()
-    TempImageType = type(img)
 
-    # create filters for crop
+    # Rescale from 0 to max-min
+    rescaler = itk.RescaleIntensityImageFilter[ImageType, OutputImageType].New()
+    rescaler.SetOutputMinimum(0)
+    rescaler.SetOutputMaximum(maxv - minv)
+    rescaler.SetInput(img)
+
+    # Filters for crop
     LabelType = itk.ctype('unsigned long')
     LabelObjectType = itk.StatisticsLabelObject[LabelType, Dim]
     LabelMapType = itk.LabelMap[LabelObjectType]
-    converter = itk.LabelImageToLabelMapFilter[TempImageType, LabelMapType].New()
-    converter.SetBackgroundValue(bg);
-    converter.SetInput(img);
+    converter = itk.LabelImageToLabelMapFilter[OutputImageType, LabelMapType].New()
+    converter.SetBackgroundValue(bg)
+    converter.SetInput(rescaler.GetOutput())
     autocrop = itk.AutoCropLabelMapFilter[LabelMapType].New()
     autocrop.SetInput(converter.GetOutput())
-    remap = itk.LabelMapToLabelImageFilter[LabelMapType, TempImageType].New()
+    remap = itk.LabelMapToLabelImageFilter[LabelMapType, OutputImageType].New()
     remap.SetInput(autocrop.GetOutput())
 
-    # recast to initial type
-    endcaster = itk.CastImageFilter[TempImageType, ImageType].New()
-    endcaster.SetInput(remap.GetOutput())
+    # recast to initial type and range
+    end_rescaler = itk.RescaleIntensityImageFilter[OutputImageType, ImageType].New()
+    end_rescaler.SetOutputMinimum(minv)
+    end_rescaler.SetOutputMaximum(maxv)
+    end_rescaler.SetInput(remap.GetOutput())
 
     # Go !
-    endcaster.Update()
-    output = endcaster.GetOutput()
-
-    # Offset if negative
-    if minv<0:
-        img_array = itk.array_view_from_image(output)
-        img_array += minv
+    end_rescaler.Update()
+    output = end_rescaler.GetOutput()
 
     return output
-
 
 
 def image_crop_with_bb(img, bb):
@@ -110,26 +102,28 @@ def image_crop_with_bb(img, bb):
     if len(dims) != 3:
         logger.error('only 3D image supported')
         exit(0)
-    #inclusive
-    from_index = np.maximum(np.zeros(3,dtype=int),np.array(img.TransformPhysicalPointToIndex(bb.mincorner)))
-    #exclusive
-    to_index = np.minimum(dims,np.array(img.TransformPhysicalPointToIndex(bb.maxcorner))+1)
+    # inclusive
+    from_index = np.maximum(np.zeros(3, dtype=int), np.array(img.TransformPhysicalPointToIndex(bb.mincorner)))
+    # exclusive
+    to_index = np.minimum(dims, np.array(img.TransformPhysicalPointToIndex(bb.maxcorner)) + 1)
     cropper = itk.RegionOfInterestImageFilter.New(Input=img)
     region = cropper.GetRegionOfInterest()
-    indx=region.GetIndex()
-    size=region.GetSize()
+    indx = region.GetIndex()
+    size = region.GetSize()
     for j in range(3):
-        indx.SetElement(j,int(from_index[j]))
-        size.SetElement(j,int(to_index[j]-from_index[j]))
+        indx.SetElement(j, int(from_index[j]))
+        size.SetElement(j, int(to_index[j] - from_index[j]))
     region.SetIndex(indx)
     region.SetSize(size)
     cropper.SetRegionOfInterest(region)
     cropper.Update()
     return cropper.GetOutput()
 
+
 #####################################################################################
 import unittest
 from .logging_conf import LoggedTestCase
+
 
 class Test_Crop(LoggedTestCase):
     def test_auto_crop(self):
@@ -153,14 +147,17 @@ class Test_Crop(LoggedTestCase):
         image.Allocate()
         image.FillBuffer(0)
         npView = itk.array_from_image(image)
-        npView[10:52, 42:192, 124:147] =1
+        npView[10:52, 42:192, 124:147] = 1
         image = itk.image_from_array(npView)
         autoCrop = image_auto_crop(image)
         autoCropSize = autoCrop.GetLargestPossibleRegion().GetSize()
         self.assertTrue(np.allclose(autoCropSize[0], 23))
         self.assertTrue(np.allclose(autoCropSize[1], 150))
         self.assertTrue(np.allclose(autoCropSize[2], 42))
+        print(image.GetOrigin())
+        print(autoCrop.GetOrigin())
         self.assertTrue(np.allclose(itk.array_from_image(autoCrop)[0, 0, 0], 1))
+
     def test_crop(self):
         x = np.arange(-10, 10, 0.1)
         y = np.arange(-12, 15, 0.1)
