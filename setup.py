386aa00d9cd0032761bb4384db50c9a615efdda7
Thomas BAUDIER <thomas.baudier@creatis.insa-lyon.fr>
2021-01-06 11:54:37 +0100

    v0.9.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="gatetools",
-    version="0.9.11",
+    version="0.9.12",
     author="OpenGate collaboration",
     author_email="david.sarrut@creatis.insa-lyon.fr",
     description="Python tools for GATE, see https://github.com/OpenGATE/Gate",
