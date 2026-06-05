7472a5d0399a60190df5a2342907e67c46a6bc0a
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2026-06-05 13:45:13 +0200

    data cleaning to handle NaN values in gt_phsp_plot

--- gatetools/bin/gt_phsp_plot.py
+++ gatetools/bin/gt_phsp_plot.py
@@ -16,24 +16,49 @@ import math
 
 logger = logging.getLogger(__name__)
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.argument('filenames', nargs=-1)
-@click.option('-n', default=float(1e5), help='Use -1 to read all data')
-@click.option('--keys', '-k', help='Plot the given keys (as a str list such that "X Y Z")', default='')
-@click.option('--skip', multiple=True, help='(string) Dont plot if this str is contained in a branch name')
-@click.option('--quantile', '-q', default=float(0), help='Restrict histogram to quantile')
-@click.option('--nb_bins', '-b', default=int(100), help='Number of bins')
-@click.option('--tree', '-t', default='PhaseSpace', help='Name of the tree in the root file')
-@click.option('--shuffle', '-s', default=False, is_flag=True, help='shuffle samples when loading')
-@click.option('--output', '-o', type=str, help='Do not plot, only output a pdf with the given name')
-@click.option('--plot2d',
-              type=(str, str),
-              help='Add 2D plots (key1,key2), such as --plot2d X Ekine --plot2d X Y ', multiple=True)
+@click.argument("filenames", nargs=-1)
+@click.option("-n", default=float(1e5), help="Use -1 to read all data")
+@click.option(
+    "--keys",
+    "-k",
+    help='Plot the given keys (as a str list such that "X Y Z")',
+    default="",
+)
+@click.option(
+    "--skip",
+    multiple=True,
+    help="(string) Dont plot if this str is contained in a branch name",
+)
+@click.option(
+    "--quantile", "-q", default=float(0), help="Restrict histogram to quantile"
+)
+@click.option("--nb_bins", "-b", default=int(100), help="Number of bins")
+@click.option(
+    "--tree", "-t", default="PhaseSpace", help="Name of the tree in the root file"
+)
+@click.option(
+    "--shuffle", "-s", default=False, is_flag=True, help="shuffle samples when loading"
+)
+@click.option(
+    "--output",
+    "-o",
+    type=str,
+    help="Do not plot, only output a pdf with the given name",
+)
+@click.option(
+    "--plot2d",
+    type=(str, str),
+    help="Add 2D plots (key1,key2), such as --plot2d X Ekine --plot2d X Y ",
+    multiple=True,
+)
 @gt.add_options(gt.common_options)
-def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, skip, output, **kwargs):
+def gt_phsp_plot(
+    filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, skip, output, **kwargs
+):
     """
     \b
     Plot histograms
@@ -62,7 +87,7 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
         data, read_keys, m = phsp.load(filename, tree, n, shuffle)
         if n == -1:
             n = m
-        print(f'Reading {n}/{m}')
+        print(f"Reading {n}/{m}")
 
         # get keys
         ckeys = phsp.str_keys_to_array_keys(keys)
@@ -78,7 +103,7 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
                 add = True
                 for sk in skip_branches:
                     if sk in k:
-                        print('Skip branch ', k)
+                        print("Skip branch ", k)
                         add = False
                 if add:
                     fk.append(k)
@@ -102,39 +127,51 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
         nfig = 0
         for k in first_keys:
             if k not in read_keys:
-                print(f'Skip key {k}: not in the first list of keys')
+                print(f"Skip key {k}: not in the first list of keys")
                 continue
 
             # get data
             index = read_keys.index(k)
             x = data[:, index]
 
+            # Try to convert to float (handles object arrays containing numeric values and nan)
+            try:
+                x = x.astype(float)
+            except (ValueError, TypeError):
+                pass
+
+            # Filter out NaN values
+            try:
+                x = x[~np.isnan(x)]
+            except TypeError:
+                pass
+
             if len(x) < 1:
-                print(f'Skip key {k}: empty')
+                print(f"Skip key {k}: empty (or all NaN)")
                 continue
 
             # check validity
             if type(x[0]) == str:
-                print(f'Skip key {k} : str')
+                print(f"Skip key {k} : str")
                 continue
             try:
                 a = int(x[0])
             except:
-                print(f'Skip key {k}: not numeric? x[0] = {x[0]}')
+                print(f"Skip key {k}: not numeric? x[0] = {x[0]}")
                 continue
             # sometimes, if x is a str (from a root file), x[0] will be 'NULL'
             # (probably not the best method ; to be changed)
-            if x[0] == 'NULL':
-                print(f'Skip key {k} : not numeric? x[0] = NUL')
+            if x[0] == "NULL":
+                print(f"Skip key {k} : not numeric? x[0] = NUL")
                 continue
 
             # get mean to check if nan
             xmean = np.mean(x)
             xmax = np.max(x)
             xmin = np.min(x)
-            print(f'Key {k} min/mean/max: {xmin} {xmean} {xmax}')
+            print(f"Key {k} min/mean/max: {xmin} {xmean} {xmax}")
             if np.isnan(xmean):
-                print(f'Skip key {k} : nan ?')
+                print(f"Skip key {k} : nan ?")
                 continue
 
             a = phsp.fig_get_sub_fig(ax, i)
@@ -145,16 +182,21 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
             if k not in q:
                 q[k] = (np.quantile(x, q1), np.quantile(x, q2))
 
-            label = ' {} $\\mu$={:.2f} $\\sigma$={:.2f}'.format(k, np.mean(x), np.std(x))
-            a.hist(x, nb_bins,
-                   # density=True,
-                   histtype='stepfilled',
-                   range=q[k],
-                   # facecolor='g',
-                   alpha=0.5,
-                   label=label)
+            label = " {} $\\mu$={:.2f} $\\sigma$={:.2f}".format(
+                k, np.mean(x), np.std(x)
+            )
+            a.hist(
+                x,
+                nb_bins,
+                # density=True,
+                histtype="stepfilled",
+                range=q[k],
+                # facecolor='g',
+                alpha=0.5,
+                label=label,
+            )
             # a.set_ylabel('Probability')
-            a.set_ylabel('Counts')
+            a.set_ylabel("Counts")
             a.legend()
             i = i + 1
             nfig += 1
@@ -162,7 +204,7 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
         # 2D
         for k in keys_2D:
             a = phsp.fig_get_sub_fig(ax, i)
-            phsp.fig_histo2D(a, data, read_keys, k, nb_bins, 'g')
+            phsp.fig_histo2D(a, data, read_keys, k, nb_bins, "g")
             i = i + 1
 
     if nb_fig == 0:
@@ -179,7 +221,7 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
     n = int(n)
     m = int(m)
     # plt.subplots_adjust(top=0.7)
-    plt.suptitle(f'Values: {n}/{m}')
+    plt.suptitle(f"Values: {n}/{m}")
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     if output:
         plt.savefig(output)
@@ -189,5 +231,5 @@ def gt_phsp_plot(filenames, keys, n, quantile, tree, nb_bins, plot2d, shuffle, s
 
 
 # --------------------------------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     gt_phsp_plot()
